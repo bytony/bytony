4ae5f1178485b2c8dbf5a044c29c132b3aac36a4
bytony <6218796@qq.com>
2013-12-17 21:49:03 +0800

    更新代理页面views层

---
+++ Lanrain/Lib/Action/System/AgentAction.class.php
@@ -0,0 +1,35 @@
+<?php
+class AgentAction extends BackAction{
+	public function _initialize() {
+        parent::_initialize();  //RBAC 验证接口初始化
+    }
+	
+	public function index(){
+		$this->display();
+	}
+	public function email(){
+		$this->display();
+	}	
+	public function alipay(){
+		$this->display();
+	}
+	public function safe(){
+		$this->display();
+	}
+	public function upfile(){
+		$this->display();
+	}
+	public function insert(){
+
+			$data=D('agent');
+			$where = array('site_url'=>$this->_post('site_url'));
+			$check = $data->where($where)->find();
+			if($check==false) $this->error('非法操作');
+		 	if($data->where($where)->save($_POST)){
+		 	$this->success('操作成功',U('Site/index',array('pid'=>6,'level'=>3)));
+			 }else{
+			$this->success('操作失败',U('Site/index',array('pid'=>6,'level'=>3)));
+		}
+	}
+
+}

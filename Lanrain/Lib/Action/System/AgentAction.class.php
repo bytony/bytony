6311bfc53fa023caae2041eec5cc806d36ddc059
bytony <6218796@qq.com>
2013-12-21 13:55:49 +0800

    更新代理功能

--- Lanrain/Lib/Action/System/AgentAction.class.php
+++ Lanrain/Lib/Action/System/AgentAction.class.php
@@ -23,12 +23,14 @@ class AgentAction extends BackAction{
 
 			$data=D('agent_info');
 			$data2=D('agent');
+			$data3=D('user');
+			$user['username'] = $_POST['agentname'];
+			$user['password'] = md5($_POST['agentpwd']);
 			$agentinfo['site_url']=$_POST['agenturl'];
-		 	if($data->add($_POST)&&($data2->add($agentinfo))){
+		 	if($data->add($_POST)&&($data2->add($agentinfo))&&($data3->add($user))){
 		 	$this->success('操作成功',U('Agent/index',array('pid'=>6,'level'=>3)));
 			 }else{
 			$this->success('操作失败',U('Agent/index',array('pid'=>6,'level'=>3)));
 		}
 	}
-
 }
